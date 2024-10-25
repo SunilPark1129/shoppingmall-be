076d31354bc505057ff6397258620c2a6ad91ee4
Sunil Park <sunilpark1129@gmail.com>
2024-10-25 07:54:11 -0400

    fixed - typo

--- routes/user.api.js
+++ routes/user.api.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const userController = require("../controllers/user.controller");
-const authController = require("../controllers/auth,controller");
+const authController = require("../controllers/auth.controller");
 const router = express.Router();
 
 router.post("/", userController.createUser);
