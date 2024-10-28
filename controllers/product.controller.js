322e34cce335d7fc74d94331fa640f38d6b4816f
Sunil Park <sunilpark1129@gmail.com>
2024-10-28 03:02:01 -0400

    added - product api get

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -35,4 +35,13 @@ productController.createProduct = async (req, res) => {
   }
 };
 
+productController.getProduct = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ status: "success", data: products });
+  } catch (error) {
+    res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
 module.exports = productController;
