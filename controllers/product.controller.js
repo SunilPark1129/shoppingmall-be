627acad4a76bfcd76fac4a62e336709bd98d0b2d
Sunil Park <sunilpark1129@gmail.com>
2024-10-28 05:05:05 -0400

    added - search and pagenation api

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,6 +1,7 @@
 const Product = require("../models/Product");
 
 const productController = {};
+const PAGE_SIZE = 5;
 
 productController.createProduct = async (req, res) => {
   try {
@@ -37,8 +38,40 @@ productController.createProduct = async (req, res) => {
 
 productController.getProduct = async (req, res) => {
   try {
-    const products = await Product.find({});
-    res.status(200).json({ status: "success", data: products });
+    let { page, name } = req.query;
+    page = page || 1;
+
+    const cond = name
+      ? {
+          name: { $regex: name, $options: "i" },
+        }
+      : {};
+
+    let query = Product.find(cond);
+    const response = { status: "success" };
+
+    if (page) {
+      // skip과 limit은 몽구스에서 서포트하는 함수
+      // skip  - 앞의 아이템들을 몇개 스킵할 것인지 정함
+      // limit - 최대 몇개의 아이템들을 포함할지 정함
+
+      // (page-1)*5 -> 해당 페이지부터 5개의 페이지를 가져옴
+      query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+
+      // 전체 페이지 구하기
+      // 1. 데이터가 총 몇개인지 구함
+      const totalItemNum = await Product.find(cond).countDocuments(); // count()는 더이상 지원하지 않고 countDocuments로 전체 숫자만 가져옴
+
+      // 2. 데이터 총 개수 / PAGE_SIZE
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+    }
+
+    // exec() 실행하고 싶을때 사용
+    const productList = await query.exec();
+    response.data = productList;
+
+    res.status(200).json({ ...response });
   } catch (error) {
     res.status(400).json({ status: "fail", error: error.message });
   }
