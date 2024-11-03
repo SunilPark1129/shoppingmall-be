afb6e4bd43560b080e45beae2e4086a615f271b3
Sunil Park <sunilpark1129@gmail.com>
2024-11-03 09:13:24 -0500

    added - get order lists

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -3,6 +3,7 @@ const { randomStringGenerator } = require("../utils/randomStringGenerator");
 const productController = require("./product.controller");
 
 const orderController = {};
+const PAGE_SIZE = 8;
 
 orderController.createOrder = async (req, res) => {
   try {
@@ -38,6 +39,43 @@ orderController.createOrder = async (req, res) => {
     });
     await newOrder.save();
     res.status(200).json({ status: "success", orderNum });
+  } catch (error) {
+    res.status(400).json({ status: "fail", message: error.message });
+  }
+};
+
+orderController.getOrder = async (req, res) => {
+  try {
+    const { userId } = req;
+
+    const orderList = await Order.find({ userId }).populate({
+      path: "items",
+      populate: {
+        path: "productId",
+        model: "Product",
+        select: "image name",
+      },
+    });
+    const totalItemNum = await Order.find({ userId }).countDocuments();
+
+    const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+    console.log("start");
+
+    console.log(totalPageNum);
+    res.status(200).json({ status: "success", data: orderList, totalPageNum });
+  } catch (error) {
+    return res.status(400).json({ status: "fail", error: error.message });
+  }
+};
+
+orderController.getOrderList = async (req, res) => {
+  try {
+    const { userId } = req;
+    const orderList = await Order.findOne({ userId });
+
+    if (!orderList) throw new Error("주문하신 오더가 없습니다");
+
+    res.status(200).json({ status: "success", data: orderList });
   } catch (error) {
     res.status(400).json({ status: "fail", message: error.message });
   }
--- routes/order.api.js
+++ routes/order.api.js
@@ -4,5 +4,7 @@ const orderController = require("../controllers/order.controller");
 const router = express.Router();
 
 router.post("/", authController.authenticate, orderController.createOrder);
+router.get("/me", authController.authenticate, orderController.getOrder);
+// router.get("/", authController.authenticate, orderController.getOrderList);
 
 module.exports = router;
